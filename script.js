f7ee5c1d1307d9809a8440a97233c06e7500640a
Jimin730 <dnddnd120@naver.com>
2025-04-29 09:37:56 +0900

    포트폴리오

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+// 스무스 스크롤링
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
